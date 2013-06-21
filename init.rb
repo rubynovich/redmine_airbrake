8e1731d33a6ffd2d041d5305ec86cbb41667fafe
rubynovich <roman@shipiev.me>
2013-06-21 13:51:09 +0400

    first and last commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_airbrake do
+  name 'Airbrake'
+  author 'Roman Shipiev'
+  description 'gem "airbrake"'
+  version '0.0.1'
+  url 'https://bitbucket.org/rubynovich/redmine_airbrake'
+  author_url 'http://roman.shipiev.me'
+end
